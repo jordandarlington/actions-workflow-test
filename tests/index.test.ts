0b8d60215540eb1db13954cbe503dfb190ef6567
Jordan Darlington <jordan.darlington@ii.co.uk>
2025-03-03 12:36:29 +0000

    adds ts file & adds unit tests

---
+++ tests/index.test.ts
@@ -0,0 +1,13 @@
+import { sum, subtract, multiply } from '../src/index';
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('subtracts 2 - 1 to equal 1', () => {
+    expect(subtract(2, 1)).toBe(1);
+});
+
+test('multiplies 2 * 2 to equal 4', () => {
+    expect(multiply(2, 2)).toBe(4);
+});

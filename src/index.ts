f72ec958cbe6e66e2d7b4c4c87e24885c248b211
Jordan Darlington <jordan.darlington@ii.co.uk>
2025-03-04 15:48:01 +0000

    adds comment to index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,5 @@ export const subtract = (a: number, b: number): number => {
 export const multiply = (a: number, b: number): number => {
     return a * b;
 };
+
+// This comment should only exist on the develop branch
